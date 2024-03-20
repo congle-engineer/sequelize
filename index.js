0c612f1babdbad6b64ca74658727bead53fad4d7
congle-engineer <congle-engineer@gmail.com>
2024-03-20 16:36:20 +0700

    Create a new user and list all users

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 const express = require('express');
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
+const bodyParser = require('body-parser');
 
 const host = process.env.host;
 const dbName = process.env.dbName;
@@ -13,7 +14,20 @@ const app = express();
 
 const sequelize = new Sequelize(dbName, userName, password, {
   host: host,
-  dialect: 'postgres'
+  dialect: 'postgres',
+  define: {
+    freezeTableName: true
+  }
+});
+
+const User = sequelize.define("user", {
+  name: DataTypes.TEXT,
+  favoriteColor: {
+    type: DataTypes.TEXT,
+    defaultValue: 'green'
+  },
+  age: DataTypes.INTEGER,
+  cash: DataTypes.INTEGER
 });
 
 (async () => {
@@ -21,13 +35,40 @@ const sequelize = new Sequelize(dbName, userName, password, {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
 
+    await sequelize.sync({ force: false });
+    
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    app.use(bodyParser.json());
+
     app.get('/', (req, res) => {
       res.send('Hello World!');
+    });
+
+    app.get('/users', async (req, res) => {
+      const items = await User.findAll();
+      let result = [];
+      items.forEach(item => {
+        result.push(item.dataValues);
+      });
+      res.send(result);
     })
+
+    app.post('/user/create', async (req, res) => {
+      const { body } = req;
+      console.log('body: ', req.body);
+      const item = await User.create({
+        name: body.name,
+        favoriteColor: body.favoriteColor,
+        age: body.age,
+        cash: body.cash
+      });
+      res.send(item.dataValues);
+    });
 
     app.listen(port, () => {
       console.log(`Express started listening on port ${port}`);
-    })
+    });
   } catch (error) {
     console.error('Unable to connect to the database: ', error);
   }
